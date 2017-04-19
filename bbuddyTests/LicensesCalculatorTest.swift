0258c9ae5de43a393229ef52a33d3849baea0144
Jack KY Chen <jackkychen@kkstream.com.tw>
2017-04-19 17:34:09 +0800

    add test case for calculator

--- bbuddyTests/LicensesCalculatorTest.swift
+++ bbuddyTests/LicensesCalculatorTest.swift
@@ -35,16 +35,37 @@ class LicensesCalculatorTest: XCTestCase {
     }
     
     func testSingleMonthPrice() {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let duration = self.getFakeDuration(fromDate: "2017-4-20", toDate: "2017-4-23")
         
-        let startDate = dateFormatter.date(from: "2017-4-20")
-        let endTime = dateFormatter.date(from: "2017-4-23")
-        let singleMonthDur = TimeDuration(startTime: startDate, endTime: endTime)
-        
-        let price = calculator.calculateForTotalAmountFromDuration(duration: singleMonthDur, licenses: calculator.fakePrice())
+        let price = calculator.calculateForTotalAmountFromDuration(duration: duration, licenses: calculator.fakePrice())
         
         assert(price == 4)
     }
     
+    func testCrossMonthPrice() {
+
+        let duration = self.getFakeDuration(fromDate: "2017-5-30", toDate: "2017-6-2")
+        
+        let price = calculator.calculateForTotalAmountFromDuration(duration: duration, licenses: calculator.fakePrice())
+        
+        assert(price == 2)
+    }
+    
+    func testWrongDurationPrice() {
+        
+        let duration = self.getFakeDuration(fromDate: "2017-4-20", toDate: "2000-4-23")
+        
+        let price = calculator.calculateForTotalAmountFromDuration(duration: duration, licenses: calculator.fakePrice())
+        
+        assert(price == 0)
+    }
+    
+    private func getFakeDuration(fromDate: String, toDate:String) -> TimeDuration {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        let startDate = dateFormatter.date(from: fromDate)
+        let endTime = dateFormatter.date(from: toDate)
+        return TimeDuration(startTime: startDate, endTime: endTime)
+    }
 }
