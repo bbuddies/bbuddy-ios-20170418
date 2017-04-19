f6a5949a5ba112384e6acfab7c0782a78f28a33a
Hardy Liu <hardyliu@kkstream.com.tw>
2017-04-19 15:09:27 +0800

    Complete calculate subscription view without really calculate subscribe fee.

---
+++ bbuddy/SubscriptionCalculator.swift
@@ -0,0 +1,16 @@
+//
+//  SubscriptionCalculator.swift
+//  bbuddy
+//
+//  Created by Hardy Liu on 2017/4/19.
+//  Copyright © 2017年 odd-e. All rights reserved.
+//
+
+import Foundation
+
+class SubscriptionCalculator {
+    class func calculate(startDate: Date, endDate: Date) -> Float {
+        return 100.0;
+    }
+}
+
