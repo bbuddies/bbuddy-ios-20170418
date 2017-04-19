03cf2da6029d83d45fdf62a280d3855e2268d10f
Hardy Liu <hardyliu@kkstream.com.tw>
2017-04-19 14:34:00 +0800

    Complete subscribe view UI.

---
+++ bbuddy/SubscribeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SubscribeViewController.swift
+//  bbuddy
+//
+//  Created by Hardy Liu on 2017/4/19.
+//  Copyright © 2017年 odd-e. All rights reserved.
+//
+
+import UIKit
+
+class SubscribeViewController: UIViewController {
+
+    @IBOutlet weak var calulateSubscriptionPlanButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func setupAccessibility() {
+        calulateSubscriptionPlanButton.accessibilityIdentifier = "calculate_subscription_plan_button"
+    }
+}
